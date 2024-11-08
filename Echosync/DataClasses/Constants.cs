e8e9f156767003e07f85cee25997b13ea641f722
j.katzwinkel@posteo.de <j.katzwinkel@posteo.de>
2024-11-08 08:33:53 +0100

    Remove old data

--- Echosync/DataClasses/Constants.cs
+++ Echosync/DataClasses/Constants.cs
@@ -8,8 +8,5 @@ namespace Echosync.DataClasses
         public static readonly Vector4 INFOLOGCOLOR = new Vector4(.3f, 1.0f, 1.0f, 1f);
         public static readonly Vector4 DEBUGLOGCOLOR = new Vector4(0.0f, 1.0f, 0.0f, 1f);
         public static readonly Vector4 ERRORLOGCOLOR = new Vector4(1.0f, 0.0f, 0.0f, 1f);
-        public static string RabbitMQConnectionUrl = "https://echosync.hogwarts-library.de";
-        public static string RabbitMQUserName = "Echosync-User";
-        public static string RabbitMQPassword = "1234432112344321";
     }
 }
